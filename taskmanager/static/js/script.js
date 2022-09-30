bd9034fca539878f86855534f32b4bc1b579a851
Koby1705 <koby1705@gmail.com>
2022-09-30 19:01:12 +0000

    Update categories

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,4 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
-  });
+});
